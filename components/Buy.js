6c19404cf5af4548c378d4cd4dfac2934035de70
proyceus <proyceus@gmail.com>
2022-06-24 18:16:15 -0400

    added component to purchase items

---
+++ components/Buy.js
@@ -0,0 +1,25 @@
+import React, { useState, useMemo } from "react";
+import { Keypair, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { InfinitySpin } from "react-loader-spinner";
+import IPFSDownload from "./IpfsDownload";
+
+const Buy = ({ itemID }) => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  const orderID = useMemo(() => Keypair.generate().publicKey, []);
+
+  const [paid, setPaid] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const order = useMemo(
+    () => ({
+      buyer: publicKey.toString(),
+      orderID: orderID.toString(),
+      itemID: itemID,
+    }),
+    [publicKey, orderID, itemID]
+  );
+};
+
+export default Buy;
