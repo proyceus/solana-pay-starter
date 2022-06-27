80d801cd2139580682d086584d5110ae9f906b32
proyceus <proyceus@gmail.com>
2022-06-27 14:28:46 -0400

    added buy button that links to mafia pack

--- components/Buy.js
+++ components/Buy.js
@@ -20,6 +20,65 @@ const Buy = ({ itemID }) => {
     }),
     [publicKey, orderID, itemID]
   );
+
+  const processTransaction = async () => {
+    setLoading(true);
+    const txResponse = await fetch("../api/createTransaction", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    });
+    const txData = await txResponse.json();
+
+    const tx = Transaction.from(Buffer.from(txData.transaction, "base64"));
+    console.log("Tx data is", tx);
+
+    try {
+      const txHash = await sendTransaction(tx, connection);
+      console.log(
+        `Transaction sent: https://solscan.io/tx/${txHash}?cluster=devnet`
+      );
+      setPaid(true);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (!publicKey) {
+    return (
+      <div>
+        <p>You need to connect your wallet to make transactions</p>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return <InfinitySpin color="gray" />;
+  }
+
+  return (
+    <div>
+      {paid ? (
+        <IPFSDownload
+          filename="mafiapack.zip"
+          hash="QmdgkZfev6Ugru2Jsr9N9EAL3dpVL4PNSgpRGA8x54SQ1K"
+          cta="Download mafia pack"
+        />
+      ) : (
+        <button
+          disabled={loading}
+          className="buy-button"
+          onClick={processTransaction}
+        >
+          Buy now
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Buy;
